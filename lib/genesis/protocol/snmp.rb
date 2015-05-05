bd0208bfc7ff628c9d2e91a3186105c216db087c
Dale Hamel <dale.hamel@shopify.com>
2015-05-05 11:24:18 -0400

    Give snmp some love

--- lib/genesis/protocol/snmp.rb
+++ lib/genesis/protocol/snmp.rb
@@ -1,3 +1,17 @@
+require 'snmp'
+
 require 'genesis/protocol/snmp/protocol'
 require 'genesis/protocol/snmp/server'
 require 'genesis/protocol/snmp/handler'
+
+module Genesis
+  # Common helper methods for protocol
+  module Snmp
+    # Send an SNMP trap
+    def send_trap(sys_up_time, trap_oid, object_list=[], **kwargs)
+      SNMP::Manager.open(host: kwargs[:host] || '127.0.0.1', trapport: kwargs[:port] || 161) do |manager|
+        puts manager.trap_v2(sys_up_time, trap_oid, object_list)
+      end
+    end
+  end
+end
--- lib/genesis/protocol/snmp/server.rb
+++ lib/genesis/protocol/snmp/server.rb
@@ -14,12 +14,8 @@ module Genesis
         SNMP::TrapListener.new(host: 0, port: @port, community: commstr) do |manager|
           manager.on_trap_default do |snmp_trap|
             EM.defer do
-              # Add routes here
-              # Also put into channel
-              # Test handler for channel
-              puts snmp_trap
+              # FIXME: Add routing to trap handlers here
               @channel << snmp_trap
-              puts 'trapped'
             end
           end
         end
@@ -27,7 +23,3 @@ module Genesis
     end
   end
 end
-# To test:
-# SNMP::Manager.open(Host: '127.0.0.1', TrapPort: 1061) do |manager|
-#   puts manager.trap_v2(1234, "1.3.6.1.2.3.4", ["1.2.3", "1.4.5.6"])
-# end
